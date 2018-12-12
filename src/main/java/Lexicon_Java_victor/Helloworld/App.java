c9d29fdce7e38222a425a08305a2d312a09ca993
Victor_506 <viho98001@utb.vaxjo.se>
2018-12-12 09:59:03 +0100

    hello world

---
+++ src/main/java/Lexicon_Java_victor/Helloworld/App.java
@@ -0,0 +1,13 @@
+package Lexicon_Java_victor.Helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World how are you?!" );
+    }
+}
